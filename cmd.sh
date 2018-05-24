042239bd2c4d259807560657bd4136a1ba32ea87
ysj_88 <sjyeo88@gmail.com>
2018-05-24 09:43:31 +0900

    Debug lecture directory2

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,4 @@ sudo  docker stop express-mobile
 sudo  docker rm express-mobile
 sudo npm install
 sudo docker build --tag express-mobile .
-sudo docker run -p 3001:3001 --name express-mobile --volume /var/dmhc/assets:/app/assets  --link db-vol express-mobile
+sudo docker run -p 3001:3001 --name express-mobile --volume /var/dmhc/assets:/app/assets --volume /var/dmhc/assets-expert:/app/assets-expert  --link db-vol express-mobile
